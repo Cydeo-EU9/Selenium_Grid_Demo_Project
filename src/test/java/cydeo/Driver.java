a5b2d5f7499cb4e1bb33087b748257cb7505b3d5
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-10-15 02:54:23 +0300

    chrome is changed to remote-chrome

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "52.90.101.17";
+                        String gridAddress = "34.207.126.76";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
